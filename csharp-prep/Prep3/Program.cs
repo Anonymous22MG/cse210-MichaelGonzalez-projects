c3a44066cc97b4ea81991d7130af78343333a9f5
Michael Gonzalez <mgtech22@byui.edu>
2026-01-14 22:33:05 -0700

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,27 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        int mnumber = int.Parse(Console.ReadLine());
+        string playAgain = "yes";
 
-        Console.Write("What is your guess? ");
-        int guess = int.Parse(Console.ReadLine());
-
-        while (guess != mnumber)
+        while (playAgain.ToLower() == "yes")
         {
-            if (guess > mnumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("Higher");
-            }
+            Random randomGenerator = new Random();
+            int mnumber = randomGenerator.Next(1, 101);
+            int guessCount = 0;
 
             Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
-        }
+            int guess = int.Parse(Console.ReadLine());
+            guessCount++;
 
-        Console.WriteLine("You guessed it!");
+            while (guess != mnumber)
+            {
+                if (guess > mnumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+            }
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
+
