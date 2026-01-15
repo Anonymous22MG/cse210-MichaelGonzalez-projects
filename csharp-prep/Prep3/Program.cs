18844d826133fc3069c40097bab5b2e00f5b230f
Michael Gonzalez <mgtech22@byui.edu>
2026-01-14 22:14:11 -0700

    in process

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int mnumber = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != mnumber)
+        {
+            if (guess > mnumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < mnumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        }
     }
 }
